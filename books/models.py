3f0c658ad0d915b3c46160aca7c257ddeed6aa7b
X00217329 Daphne Attiogbe <X00217329@mytudublin.ie>
2024-11-08 15:12:44 +0000

    lab 9 part 4 commit

--- books/models.py
+++ books/models.py
@@ -1,5 +1,3 @@
-from django.db import models
-
 import uuid
 from django.db import models
 from django.urls import reverse
@@ -13,6 +11,7 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     date_publication = models.DateField(blank=False, null=False)
+    cover = models.ImageField(upload_to='covers/', blank=True)
     
     def __str__(self):
         return self.title
